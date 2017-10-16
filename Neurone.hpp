f47d76fdfaadb0b59c655525fcbd689380511b58
JSDupre <32522531+JSDupre@users.noreply.github.com>
2017-10-16 13:46:47 +0200

    Update Neurone.hpp

--- Neurone.hpp
+++ Neurone.hpp
@@ -11,14 +11,14 @@ class Neurone {
 	std::vector<int> SpikesTimeInNumberOfTimeIncrement_; //store the spikes time (a certain number of time incrment)
 	int localClock_;                                     //local clock for the neurone (can be incremented before the global simulation clock)
 	std::vector<Connection> connections_;                //all the connection the neurone have (s
-	//buffer ??
+	std::vector<double,D> spikeRingBuffer_;		     //used to sum the different J of every presynaptic neurones who has fired after a certain delay D
 	
 	public:
 	Neurone ();
 	bool update(unsigned int const& NumberOfTimeIncrement,double const& ElectricInput,double const& InitialTime);
 	double getMembranePotential() const;
-	std::vector<double> getSpikesTime() const;
-	vector<Connection> getConnections();
+	std::vector<int> getSpikesTimeInNumberOfTimeIncrement() const;
+	vector<Connection> getConnections() const;
 	void setConnections (vector<Connection>& connections);//const& ?
 	void receive(int clockPlusDelay,double J);
 };
