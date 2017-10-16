93fa667a454791412a5bc3f734815f74dd4d1b8c
JSDupre <32522531+JSDupre@users.noreply.github.com>
2017-10-16 14:07:38 +0200

    Update Connection.hpp

--- Connection.hpp
+++ Connection.hpp
@@ -1,20 +1,20 @@
 
+#ifndef CONNECTION_H
+#define CONNECTION_H
 
 class Neurone;//predeclaration pour eviter dependance circulaire
 
 class Connection {
   private: 
     Neurone* postsynapticNeurone_;
-    double J; //connection strenght
-    unsigned int D; //delay in the transmission of the spikes
+    double J_; //connection strenght
     
    public:
-    Connection(Neurone* pre,Neurone* post,double J,unsigned int D)
+    Connection(Neurone* post,double J);
     Connection(Connection const& c)=delete;
     ~Connection();
-    getPost(); //const?
-    getJ() const;
-    getD() const;
+    Neurone* getPost(); //const?
+    double getJ() const;
 };
     
-   
+#endif
