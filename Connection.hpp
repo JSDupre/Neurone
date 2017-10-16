4597e254225300c698c8746781f46cd579d5b0f2
JSDupre <32522531+JSDupre@users.noreply.github.com>
2017-10-16 11:12:16 +0200

    Create Connection.hpp

---
+++ Connection.hpp
@@ -0,0 +1,20 @@
+
+
+class Connection {
+  private: 
+    Neurone* presynapticNeurone_;
+    Neurone* postsynapticNeurone_;
+    double J; //connection strenght
+    unsigned int D; //delay in the transmission of the spikes
+    
+   public:
+    Connection(Neurone* pre,Neurone* post,double J,unsigned int D)
+    Connection(Connection const& c)=delete;
+    ~Connection()
+    getPost
+    getPre
+    getJ
+    getD
+};
+    
+   
