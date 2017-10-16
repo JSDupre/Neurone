19356cdd578ea8f48696969ac4d4b25c6b02afd2
JSDupre <32522531+JSDupre@users.noreply.github.com>
2017-10-16 13:59:03 +0200

    Update Network.hpp

--- Network.hpp
+++ Network.hpp
@@ -3,9 +3,10 @@
 
 class Network{
    private: 
-      std::vector<Neurone> neurones_;
+      std::vector<Neurone*> neurones_;
    public:
       Network();
       BuildNetwork(unsigned int number_of_neurones, double connection_probability);
+      int run(clock,...);
       getNeurones();
  };
